480def6ef8f6eeaf65e935b5d77c6d4faa385bb9
Jinyong JO <jiny92@kisti.re.kr>
2018-10-29 18:16:47 +0900

    domain

--- templates/includes/footer.php
+++ templates/includes/footer.php
@@ -9,13 +9,13 @@
 <div id="layout-footer">
 	<div id="footer">
 		<p class="kafe">
-			<img src="images/kafe.png" alt="KAFE" />
+			<a href="https://www.kafe.or.kr" target="_blank"><img src="images/kafe.png" alt="KAFE" /></a>
 			Korean Access FEderation
 		</p>
 
 		<ul id="fnb">
-			<li><a href="https://coreen.kreonet.net/user_agreement" target="_blank">PRIVACY POLICY</a></li>
-			<li><a href="https://coreen.kreonet.net/privacy_policy" target="_blank">TERMS OF USE</a></li>
+			<li><a href="https://www.kafe.or.kr/user_agreement" target="_blank">PRIVACY POLICY</a></li>
+			<li><a href="https://www.kafe.or.kr/privacy_policy" target="_blank">TERMS OF USE</a></li>
 		</ul>
 	</div>
 </div>
